c79b74bbe2b07ff148cdc2d6eb7f9ab339af5fcd
Zaid Marrie <zaidmarrie27@gmail.com>
2024-02-21 16:13:07 +0200

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Data Structures & Algorithms Practice",
+	description:
+		"A collection of data structure and algorithm concepts that I have learned.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
